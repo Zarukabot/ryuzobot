948cfc9fac7d10262dcf3e0d9c63c4a818b1c95c
Zarukabot <zarukayaagami@gmail.com>
2025-10-23 19:43:21 +0700

    first commit

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,15 +5,15 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "100"))
 
-DEVS = list(map(int, os.getenv("DEVS", "6139972751").split()))
+DEVS = list(map(int, os.getenv("DEVS", "1221179170").split()))
 
-API_ID = int(os.getenv("API_ID", "20607074"))
+API_ID = int(os.getenv("API_ID", "25486992"))
 
-API_HASH = os.getenv("API_HASH", "f5639cb2a94b785afc60800f351ec802")
+API_HASH = os.getenv("API_HASH", "f3da3fe731705a5d72f8dce6c79c01b5")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7600571701:AAGNyYxTB5yTaGo1bfEkTc7CtatqHUOxCPk")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7674395972:AAF17Pp7dMjcdTeLbWVSqHpXxhleDfKjaUM")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "6139972751"))
+OWNER_ID = int(os.getenv("OWNER_ID", "1221179170"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002053287763 -1002044997044 -1002022625433 -1002050846285 -1002400165299 -1002416419679 -1001473548283").split()))
 
